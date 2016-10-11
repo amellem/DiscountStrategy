586721fa5fea24c98b841ac2e72342d7fbe6c40d
Aerius <Aerius@Aerius-PC>
2016-10-11 03:54:00 -0500

    Discount Strategy - was working but I broke it and I'm not sure how

--- src/asm/discountstrategy/Startup.java
+++ src/asm/discountstrategy/Startup.java
@@ -12,14 +12,16 @@ public class Startup {
     public static void main(String[] args) {
         DatabaseStrategy db = new InMemoryDatabase();
         PosTerminal pos = new PosTerminal();
-        OutputStrategy output = new ReceiptOutput();
+        OutputStrategy consoleOutput = new ReceiptOutput();
+        OutputStrategy guiOutput = new MonitorOutput();
        
        
        pos.startNewSale("100", db);
        pos.addNewProductToSale("P100", 2.0);
        pos.addNewProductToSale("P102", 4.0);
        pos.addNewProductToSale("P103", 1.0);
-       pos.endSale(output);
+       pos.endSale(consoleOutput);
+       pos.endSale(guiOutput);
         
     }
     
