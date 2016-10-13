cefbaab0af151c2c8a8e6d69d6d84b2146c87066
Aerius <Aerius@Aerius-PC>
2016-10-13 14:54:20 -0500

    complete

--- src/asm/discountstrategy/MonitorOutput.java
+++ src/asm/discountstrategy/MonitorOutput.java
@@ -19,16 +19,16 @@ public final void createReceipt(Customer customer, LineItem[] item){
     
     
     
-    receiptData += "\t\t     Kohl's\n-----------------------------------------------\n\nCustomer: " + customer.getFirstName() + " " + customer.getLastName() + "\n\n";
+    receiptData += "\t\t\t\t\t     Kohl's\n----------------------------------------------------------------------------------------------\n\nCustomer: " + customer.getFirstName() + " " + customer.getLastName() + "\n\n";
         for (LineItem i : item){
-            receiptData += i.getProduct().getName() + "\t\tQty: " + i.getProduct().getQuantity() + "\t\t" + i.getProduct().getProductID() + "\t" + convertMoney.format((i.getProduct().getUnitPrice() * i.getProduct().getQuantity()) - i.getProduct().getDiscount()) + "\n" +
-                    "    Item cost: " + convertMoney.format(i.getProduct().getUnitPrice()) + "\t    You save: " + convertMoney.format(i.getProduct().getDiscount()) + "\n\n";
+            receiptData += i.getProduct().getName() + "\t\tQty: " + i.getProduct().getQuantity() + "\t\t Product ID: " + i.getProduct().getProductID() + "\t Price Prior to Discount: " + convertMoney.format((i.getProduct().getUnitPrice() * i.getProduct().getQuantity()) - i.getProduct().getDiscount()) + "\n" +
+                    "Item cost: " + convertMoney.format(i.getProduct().getUnitPrice()) + "\t    You save: " + convertMoney.format(i.getProduct().getDiscount()) + "\n\n";
             totalBeforeDiscount += i.getProduct().getUnitPrice() * i.getProduct().getQuantity();
             totalDiscount += i.getProduct().getDiscount();            
         }
         
-        receiptData += "\t\t\t      Subtotal: " + convertMoney.format(totalBeforeDiscount) + "\n\t\t\t      Discount: " + convertMoney.format(totalDiscount) + 
-                "-\n\t\t\t\t Total: " + convertMoney.format(totalBeforeDiscount - totalDiscount) + "\n\n-----------------------------------------------\n" +
+        receiptData += "\t\t\t\t\t\t\t      Subtotal: " + convertMoney.format(totalBeforeDiscount) + "\n\t\t\t\t\t\t\t      Discount: -" + convertMoney.format(totalDiscount) + 
+                "\n\t\t\t\t\t\t\t      Total: " + convertMoney.format(totalBeforeDiscount - totalDiscount) + "\n\n----------------------------------------------------------------------------------------------\n" +
                 "\tThank you for shopping at Kohl's!\n" ;
     
    
